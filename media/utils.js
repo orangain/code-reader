76d6a3e1289201b48a4af1eaaa2529a6e2ec9995
orangain <orangain@gmail.com>
2023-12-23 16:12:16 +0900

    Add type notations

--- media/utils.js
+++ media/utils.js
@@ -1,3 +1,9 @@
+/**
+ * Get the extension of a file path.
+ *
+ * @param {string} filePath 
+ * @returns {string}
+ */
 export function extension(filePath) {
     const match = filePath.match(/\.([^\.]+)$/);
     if (match) {
@@ -6,10 +12,27 @@ export function extension(filePath) {
     return "";
 }
 
+/**
+ * Group the items in an array by a key.
+ *
+ * @template T
+ * @param {T[]} array 
+ * @param {(T) => string | number | symbol} getKey 
+ * @returns {[string | number | symbol, T[]][]}
+ */
 export function groupBy(array, getKey) {
+    /**
+     * @type {[string | number | symbol, T[]][]}
+     */
     const grouped = [];
+    /**
+     * @type {string | number | symbol | null}
+     */
     let lastKey = null;
-    let lastGroup = null;
+    /**
+     * @type {T[]}
+     */
+    let lastGroup = [];
     for (const item of array) {
         const key = getKey(item);
         if (key !== lastKey) {
