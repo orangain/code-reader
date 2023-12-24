5344e89fe186664d37d544e2e73406b5d85cffb1
orangain <orangain@gmail.com>
2023-12-24 23:36:40 +0900

    Switch viewing and editing comment

--- src/webview/components/SnippetComment.tsx
+++ src/webview/components/SnippetComment.tsx
@@ -1,6 +1,6 @@
 import { VSCodeTextArea } from "@vscode/webview-ui-toolkit/react";
 import * as types from "../../shared/store";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useLayoutEffect, useRef, useState } from "react";
 
 type SnippetCommentProps = {
   snippetId: string;
@@ -10,16 +10,41 @@ type SnippetCommentProps = {
 
 export function SnippetComment(props: SnippetCommentProps) {
   const { snippetId, comment, onChangeComment } = props;
+  const [editing, setEditing] = useState(false);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (!editing || divRef.current === null) {
+      return;
+    }
+    const textArea = divRef.current.querySelector("vscode-text-area");
+    if (textArea === null) {
+      return;
+    }
+    (textArea as any).focus();
+  }, [editing]);
 
   function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
     const commentText = event.target.value;
     onChangeComment(snippetId, commentText);
   }
 
+  if (editing) {
+    return (
+      <div ref={divRef}>
+        <VSCodeTextArea
+          value={comment?.text ?? ""}
+          style={{ width: "100%" }}
+          onChange={handleChange as any}
+          onBlur={() => setEditing(false)}
+        />
+      </div>
+    );
+  }
+
   return (
-    <VSCodeTextArea
-      value={comment?.text ?? ""}
-      onChange={handleChange as any}
-    ></VSCodeTextArea>
+    <div onClick={() => setEditing(true)} style={{ cursor: "pointer" }}>
+      {comment?.text ?? "Add a comment..."}
+    </div>
   );
 }
